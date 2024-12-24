81aa14c921973f55858ff105e14abd921908fa49
HaoNgo232 <ngogiahao2018@gmail.com>
2024-12-24 19:53:46 +0700

    113

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
 const connection = require("./config/database");
 const fileUpload = require("express-fileupload");
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -27,7 +28,29 @@ app.use("/v1/api/", apiRoutes);
 (async () => {
   //test connection
   try {
-    await connection();
+    // await connection(); // using Mongoose
+
+    // using MongoDB Driver
+    // Connection URL
+    const url = process.env.DB_HOST_WTIH_DRIVER;
+    const client = new MongoClient(url);
+
+    // Database Name
+    const dbName = process.env.DB_NAME;
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('customers');
+
+    collection.insertOne({
+      "name": "Hao Ngo",
+      "age": 30,
+      address: {
+        city: "Hanoi",
+        country: "Vietnam"
+      }
+    })
+
     app.listen(port, hostname, () => {
       console.log(`Backend zero app listening on port ${port}`);
     });
