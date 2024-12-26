1c1a68b49d653b90178bfe687d787fbe84719061
HaoNgo232 <ngogiahao2018@gmail.com>
2024-12-26 11:09:32 +0700

    Bai 115 ok

--- src/services/productService.js
+++ src/services/productService.js
@@ -5,6 +5,17 @@ createProject = async (data) => {
         let result = await Project.create(data);
         return result;
     }
+    if (data.type === "ADD-USERS") {
+        console.log("check data", data);
+        // find project by id
+        let project = await Project.findById(data.projectId).exec();
+        for (let i = 0; i < data.usersArr.length; i++) {
+            project.usersInfor.push(data.usersArr[i]);
+        }
+        let result = await project.save();
+        return result;
+    }
+    return null;
 }
 
 module.exports = {
